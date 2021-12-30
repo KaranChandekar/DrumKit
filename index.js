f8afdf75323738906892b2fc982729ad28f34861
Karan Chandekar <karanchandekar431@gmail.com>
2021-12-30 08:23:43 +0530

    make buttons work

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked")
+    })
+}
