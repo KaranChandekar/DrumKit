6b1b7d128a5fcf1f9e8eb7cc2d3d9e9fba64174c
Karan Chandekar <karanchandekar431@gmail.com>
2021-12-31 22:27:35 +0530

    make sound on keypress

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
+
+// Detecting Button Press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3")
-        tom2.play();
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3")
-        tom3.play();
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3")
-        tom4.play();
-      case "j":
-        var snare = new Audio("sounds/snare.mp3")
-        snare.play();
-      case "k":
-        var crash = new Audio("sounds/crash.mp3")
-        crash.play();
-      case "l":
-        var kickBass = new Audio("sounds/kick-bass.mp3")
-        kickBass.play();
-      default:
-        console.log(buttonInnerHTML)
-        break;
-    }
+    makeSound(buttonInnerHTML)
   });
 }
 
+
+// Detecting Keyboard Press
 document.addEventListener("keypress", function (event) {
-  console.log(event)
+  makeSound(event.key)
 })
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play();
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play();
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play();
+    case "j":
+      var snare = new Audio("sounds/snare.mp3")
+      snare.play();
+    case "k":
+      var crash = new Audio("sounds/crash.mp3")
+      crash.play();
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3")
+      kickBass.play();
+    default:
+      console.log(buttonInnerHTML)
+      break;
+  }
+}
